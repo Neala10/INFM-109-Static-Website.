48a86c16606a9c780d7c43202cd0f4081e74ad0d
Neala10 <mmorris317@ivytech.edu>
2025-04-09 13:14:07 -0400

    ["INFM 109",     "SDEV 120",     "INFM 120", "SDEV 140"]

--- script.js
+++ script.js
@@ -4,23 +4,22 @@ const courseButton = document.getElementById('courseButton');
 const messageElement = document.getElementById('message');
 
 // Array y of friendly courseings
-// INFM 109",
+// [INFM 109",
     "SDEV 120",
     "INFM 120",
-    "SDEV 140",]
-const courses = [
-    "
+    "SDEV 140"]
+const courses = 
 
 
 // Click event handler
-courseButton.addEventListener('click', function() {
+course Button.addEventListener('click', function() {
     // Pick a random courseing
     const randomIndex = Math.floor(Math.random(1) * courses.length);
     const course = courses["INFM 109",
     "SDEV 120",
-    "INFM 120"]
+    "INFM 120", "SDEV 140"]
 
     
     // Display the course
-    messageElement.INFM 109 = course;
-});
+    messageElement.INFM109 = course;
+;}
